173d64c405f4ad977f64a085fa9fe90c4e2a31e1
unknown <cuauhtemoc.ayala@proton.me>
2024-07-26 19:05:32 -0700

    added stoplight with 3 LEDs

---
+++ stoplight_LEDs/stoplight_LEDs.ino
@@ -0,0 +1,27 @@
+#define RED_LED 11
+#define YELLOW_LED 10
+#define GREEN_LED 9
+//#define POTENCIOMETER_PIN A2
+
+void setup() {
+  pinMode(RED_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(RED_LED, HIGH);
+  delay(3000);
+  digitalWrite(RED_LED, LOW);
+
+  digitalWrite(GREEN_LED, HIGH);
+  delay(5000);
+  digitalWrite(GREEN_LED, LOW);
+
+  digitalWrite(YELLOW_LED, HIGH);
+  delay(2000);
+  digitalWrite(YELLOW_LED, LOW);
+}
